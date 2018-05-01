4dd0546b5f70be1d92ceb01b6aa8f6e4017a1778
Michael Fitzgibbon <fitzgibbonm@my.lanecc.edu>
2018-05-01 06:52:32 -0700

    Added documentation and test classes

--- MTD/MTDUnitTests/BoneyardTests.cs
+++ MTD/MTDUnitTests/BoneyardTests.cs
@@ -15,6 +15,7 @@ namespace MTDUnitTests
         Domino dummyDomino;
         Boneyard boneyard12;
         Boneyard boneyard6;
+        Boneyard emptyBoneyard;
 
         [SetUp]
         public void SetUpAllTests()
@@ -22,6 +23,7 @@ namespace MTDUnitTests
             dummyDomino = new Domino(12, 12);
             boneyard12 = new Boneyard();
             boneyard6 = new Boneyard(6);
+            emptyBoneyard = new Boneyard(-1);
         }
 
         [Test]
@@ -35,7 +37,7 @@ namespace MTDUnitTests
         [Test]
         public void BoneyardGetters()
         {
-            Assert.AreEqual(92, boneyard12.DominosRemaining);
+            Assert.AreEqual(91, boneyard12.DominosRemaining);
             Assert.AreEqual(12, boneyard12[0].Side1);
         }
 
@@ -48,9 +50,43 @@ namespace MTDUnitTests
         }
 
         [Test]
-        public void BoneyardMethods()
+        public void BoneyardDraw()
         {
-            Assert.
+            Domino d1 = boneyard12[boneyard12.DominosRemaining - 1];
+            Domino d2 = boneyard12.Draw();
+            Assert.AreEqual(d1, d2);
+            
+            try
+            {
+                d1 = boneyard12[91];
+                Assert.Fail("The indexer did not throw the expected exception");
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                Assert.Pass("The indexer threw the expexted exception");
+            }
+        }
+
+        [Test]
+        public void BoneyardIsEmpty()
+        {
+            Assert.IsTrue(emptyBoneyard.IsEmpty());
+        }
+
+        [Test]
+        public void BoneyardShuffle()
+        {
+            Boneyard cloneBoneyard = boneyard12;
+            Domino d1 = cloneBoneyard[12];
+            Assert.AreEqual(cloneBoneyard[12].ToString(), d1.ToString());
+            cloneBoneyard.Shuffle();
+            Assert.AreNotEqual(cloneBoneyard[12].ToString(), d1.ToString());
+        }
+
+        [Test]
+        public void BoneyardToString()
+        {
+
         }
     }
 }
